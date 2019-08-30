5c2363a74626f2355fd1c1febf462de565e349e0
Javad Bargamadi <javadbargamadi1375@gmail.com>
2019-08-31 03:11:46 +0430

    between xml item

--- app/src/main/java/com/sematecjavaproject/cloudyweatherapp/MainActivity.java
+++ app/src/main/java/com/sematecjavaproject/cloudyweatherapp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.sematecjavaproject.cloudyweatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        Button btnChangeCity = findViewById(R.id.btnChangeCity);
+
+        btnChangeCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent searchActivityIntent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(searchActivityIntent);
+            }
+        });
     }
 }
